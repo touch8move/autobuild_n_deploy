a33c1ba269aae524c8e0c6c6bfa0b56b90983172
Dongho <backack2@gmail.com>
2017-09-05 16:17:06 +0900

    서버 실행시 tmp폴더를 읽어오기

--- index.ts
+++ index.ts
@@ -27,8 +27,8 @@ const repoDir = './tmp/';
 
 const socketserver = io(3000);
 let mysocket:SocketIO.Socket;
-let repourl = '';
-let target = '';
+// let repourl = '';
+// let target = '';
 
 const cli = (cmd:string, output:string[]):Promise<string[]> =>{
 	const cmdSub = cmd.split(' ');
@@ -53,30 +53,36 @@ const cli = (cmd:string, output:string[]):Promise<string[]> =>{
 		});
 	})
 }
-let Repo:Git.Repository;
-const getRepo = ()=>{
+let Repo:Map<string,Git.Repository> = new Map();
+
+const getRepo = (repoPath:string, repourl:string|undefined):Promise<Git.Repository>=>{
 	return new Promise((resolve, reject)=>{
-		console.log('getRepo', repoDir + target);
-		Git.Repository.open(repoDir + target)
-		.then((repo:Git.Repository)=>{
-			Repo = repo;
-			resolve(repo);
-		}).catch((err)=>{
-			console.log('no Repo', err);
+		console.log('getRepo', repoDir + repoPath);
+		let repo = Repo.get(repoPath);
+		if(repo){
+			Promise.resolve(repo)
+			.then((repo:Git.Repository)=>{
+				// Repo.push(repo);
+				resolve(repo);
+				// resolve();
+			}).catch((err)=>{
+				console.log('error', err);
+			});
+		} else {
 			if(repourl){
 				console.log('url', repourl);
-				resolve(Git.Clone.clone(repourl, path.join(__dirname, repoDir, target)));
+				resolve(Git.Clone.clone(repourl, path.join(__dirname, repoDir, repoPath)));
 			}
-		});
+		}
 	});
 }
 let commitlist: string = '';
-const repoInit = ()=>{
+const repoInit = (repoPath:string, repoUrl:string|undefined)=>{
 	commitlist = '';
 	console.log('repoInit');
-	getRepo()
+	getRepo(repoPath, repoUrl)
 	.then((repo:Git.Repository)=>{
-		Repo = repo;
+		Repo.set(repoPath, repo);
 		repo.getBranchCommit('master')
 		.then((bcommit:Git.Commit)=>{
 			console.log('entry');
@@ -95,25 +101,63 @@ const repoInit = ()=>{
 	})
 }
 
+const loadtmp = ()=>{
+	fs.readdir(repoDir, (err:Error, files:string[])=>{
+		for(var file of files){
+			console.log('dir:', file);
+		}
+		let curIndex = 0;
+		loop(curIndex, files.length, files);
+	})
+}
+
+const loopasync = (index:number, maxIndex:number) =>{
+	return new Promise((resolve, reject)=>{
+		if(index<maxIndex)
+			resolve(true);
+		else
+			reject();
+	})
+}
+const loop = (index:number, maxIndex:number, dir:string[])=>{
+	loopasync(index, maxIndex)
+	.then((hasNext)=>{
+		console.log('has', dir[index]);
+		Git.Repository.open(path.join(repoDir, dir[index]))
+		.then((repo:Git.Repository)=>{
+			console.log('repo', dir[index]);
+			Repo.set(dir[index], repo);
+			return loop(index+1, maxIndex, dir);
+		}).catch(()=>{
+			console.log('catch');
+		})
+	}).catch(()=>{
+		console.log('exit catch');
+	})
+}
+loadtmp();
 socketserver.on('connect', (socket)=>{
 	mysocket = socket;
+	let currentRepo:string;
 	console.log('connected');
 	socket.on('debug', (data:any)=>{
 		console.log('debug', data);
 	})
 	socket.on('url', (data:any)=>{
-		
-		repourl = data;
-		let tUrlA = repourl.split('/');
+		// repourl = data;
+		let tUrlA = data.split('/');
 		let last = tUrlA[tUrlA.length-1];
 		let name = last.split('.')[0];
+		let target:string;
 		console.log(last, name);
+		
 		if( name != last)
 			target = name;
 		else 
 			target = last;
+		currentRepo = target;
 		console.log('on url', target);
-		repoInit();
+		repoInit(target, data);
 		mysocket.emit('data', 'set url '+data);
 	});
 	socket.on('getData', ()=>{
@@ -128,23 +172,26 @@ socketserver.on('connect', (socket)=>{
 			return
 		}
 		let output:string[]=[];
-		build(socket, Repo, output, data);
+		let curRepo = Repo.get(currentRepo);
+		if(curRepo){
+			build(socket, curRepo, output, data);
+		}
 	});
 });
 
-const build = (socket:SocketIO.Socket, Repo:Git.Repository, output:string[], data:any)=>{
+const build = (socket:SocketIO.Socket, repo:Git.Repository, output:string[], data:any)=>{
 	let ckCommit:Git.Commit;
 	return new Promise((resolve, reject)=>{
-		Repo.getCommit(data.hash)
+		repo.getCommit(data.hash)
 		.then((commit:Git.Commit)=>{
 			ckCommit = commit;
 			console.log('git checkout');
-			return Git.Checkout.tree(Repo, ckCommit ,{checkoutStrategy:Git.Checkout.STRATEGY.SAFE});
+			return Git.Checkout.tree(repo, ckCommit ,{checkoutStrategy:Git.Checkout.STRATEGY.SAFE});
 		})
 		.then(()=>{
-			Repo.setHeadDetached(ckCommit.id());
+			repo.setHeadDetached(ckCommit.id());
 			console.log('build');
-			return cli(`docker build -f ${repoDir}${target}/Dockerfile -t ${ckCommit.sha().slice(0,5)} ${repoDir}${target}`, output);
+			return cli(`docker build -f ${repoDir}${repo.getNamespace()}/Dockerfile -t ${ckCommit.sha().slice(0,5)} ${repoDir}${repo.getNamespace()}`, output);
 		}).then((output:string[])=>{
 			console.log('add tag');
 			return cli(`docker tag ${ckCommit.sha().slice(0,5)} ${registryUrl}/${ckCommit.sha().slice(0,5)}`, output);
